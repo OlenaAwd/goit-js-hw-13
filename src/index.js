c832d302fa5e4dc323219a8c9f2a5d2324fc5924
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-07-21 10:47:01 +0300

    add pix

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import Notiflix from 'notiflix';
 // import SimpleLightbox from 'simplelightbox';
 
 
-const refs = {
-    gallery: document.querySelector('gallery'),
-    searchForm: document.querySelector('search-form'),
-};
+// const refs = {
+//     gallery: document.querySelector('gallery'),
+//     searchForm: document.querySelector('search-form'),
+// };
 
-refs.searchForm.addEventListener('submit', onSearch)
+// refs.searchForm.addEventListener('submit', onSearch)
 
-function onSearch (e) {
-    e.preventDefault();
-}
+// function onSearch (e) {
+//     e.preventDefault();
+// }
 
 fetch('https://pixabay.com/api/?key=22578440-e1e5ecfa8eecbfc5a41a583d6&image_type=photo&orientation=horizontal&safesearch=true&per_page=40') 
     .then(response => {
@@ -21,7 +21,7 @@ fetch('https://pixabay.com/api/?key=22578440-e1e5ecfa8eecbfc5a41a583d6&image_typ
     })
     .then(photo => {
         console.log(photo);
-        const markup = photoCardTemp(photoCardTemp);
+        const markup = photoCardTemp(photo);
         console.log(markup);
     })
     .catch(error => {
